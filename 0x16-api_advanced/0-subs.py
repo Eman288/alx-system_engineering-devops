46980433334712341830ccc400443f8b28c408e1
Eman288 <emantamam107@gmail.com>
2024-08-15 17:12:32 -0700

    try task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+ a function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        a function to get the number of users for the subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    
+    try:
+        r = requests.get(url, allow_redirects=False)
+
+        if r.status_code == 200:
+            data = r.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
